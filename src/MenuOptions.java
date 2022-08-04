527b5223922cf49e0beba995453a4f4c45b8c74f
[Naifat Abdallah] <[naifat.selina@mindweaver.co.uk]>
2022-08-04 02:02:36 +0100

    Add function to list the files

--- src/MenuOptions.java
+++ src/MenuOptions.java
@@ -21,5 +21,36 @@ public class MenuOptions {
         return array;
     }
 
+    //		file listing
+    protected static void listFiles() {
+
+        int fileCount = 0;
+        ArrayList<String> filenames = new ArrayList<String>();
+
+        File directoryPath = new File(System.getProperty("user.dir"));
+        File[] listOfFiles = directoryPath.listFiles();
+        fileCount = listOfFiles.length;
+
+
+        System.out.println("Files In Ascending Order: ");
+        for (int i = 0; i < fileCount; i++) {
+            if (listOfFiles[i].isFile()) {
+                filenames.add(listOfFiles[i].getName());
+            }
+        }
+
+        String[] str = new String[filenames.size()];
+
+        for (int i = 0; i < filenames.size(); i++) {
+            str[i] = filenames.get(i);
+        }
+
+        String[] sorted_filenames = sort_sub(str, str.length);
+
+        for(String currentFile: sorted_filenames) {
+            System.out.println(currentFile);
+        }
+
+    }
 
 }
