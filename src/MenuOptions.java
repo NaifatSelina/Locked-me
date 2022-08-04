b7cc3728f57c88daa2b7c2e0f9bfc87d0838dc0e
[Naifat Abdallah] <[naifat.selina@mindweaver.co.uk]>
2022-08-04 01:59:59 +0100

    Add bubble sort to sort files into ascending order

--- src/MenuOptions.java
+++ src/MenuOptions.java
@@ -5,4 +5,21 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class MenuOptions {
+
+    //sort file in ascending order
+    protected static String[] sort_sub(String array[], int size){
+        String temp = "";
+        for(int i=0; i<size; i++){
+            for(int j=1; j<(size-i); j++){
+                if(array[j-1].compareToIgnoreCase(array[j])>0){
+                    temp = array[j-1];
+                    array[j-1]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+        return array;
+    }
+
+
 }
